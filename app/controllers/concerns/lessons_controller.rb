c434bbe8cf2f2f44da254896c76efd5d30e15f53
Josie Wright <student@epicodus.com>
2014-08-25 16:53:48 -0700

    last commit

---
+++ app/controllers/concerns/lessons_controller.rb
@@ -0,0 +1,11 @@
+class lessonsController < ApplicationController
+  def index
+    @lessons = Lesson.all
+    render('lessons/index.html.erb')
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    render('lessons/show.html.erb')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'tomes#index'})
   match('tomes', {:via => :get, :to => 'tomes#index'})
+  match('tomes', {:via => :get, :to => 'tomes#show'})
+  match('tomes', {:via => :get, :to => 'tomes#show'})
+  match('tomes/:id/chapters/:id/lessons/:id', {:via => :get, :to => 'lessons#show'})
 end
