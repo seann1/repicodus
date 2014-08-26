1bd2e6416b48623c6595b829feab4984a51f200f
Josie Wright <student@epicodus.com>
2014-08-26 09:48:21 -0700

    render lessons page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'tomes#index'})
   match('tomes', {:via => :get, :to => 'tomes#index'})
   match('tomes', {:via => :get, :to => 'tomes#show'})
-  match('tomes/:id/chapters/:id/lessons/:id', {:via => :get, :to => 'lessons#show'})
+
+  match('lessons/:id', {:via => :get, :to => 'lessons#show'})
 end
